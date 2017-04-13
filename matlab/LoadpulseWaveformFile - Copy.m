f88f9078b3e55411a8be89e53a99dff012042d15
jliu4 <jliuapril@gmail.com>
2017-04-13 15:46:01 -0700

    waveform

--- matlab/LoadpulseWaveformFile - Copy.m
+++ matlab/LoadpulseWaveformFile - Copy.m
@@ -4,7 +4,7 @@ addpath('C:\jinwork\BE\matlab\export_fig\altmany-export_fig-2763b78')
 dataPath = 'D:\DropBox\Dropbox (BEC)\BECteam\Jin\waveform\';
 fn = char(strcat(dataPath,'waveform.xlsx'));
 waveform = readtable(fn);
-input = [waveform(1,:);waveform(3,:);waveform(6,:);waveform(8:9,:)];
+input = waveform(2,:);
 input.folder(1);
 n = size(input,1);
 %waveform = xlsread('waveform.xlsx');
@@ -13,7 +13,7 @@ vfactor = 0.94;% off 0.6 seconds for q-pulse measurement.
 totalTime = 0.002;
 nh = 21; %n header
 %10% from the peak value
-
+delta = 50;
 deltat = 1.00000000012417E-09;
 outputPath ='C:\jinwork\BEC\tmp\';
 filen1 = strcat(outputPath,'waveform_041217-11.csv');
@@ -23,19 +23,19 @@ figname = strcat(outputPath,'waveform_041217-11.pdf');
 delete(figname);
 pos = [10 10 1000 800];
 for wi = 1:n
-   folder = input.folder(wi);
-   dateN = input.date(wi);
-   filename = input.filename(wi);
+   folder = char(input.folder(wi));
+   dateN = char(input.date(wi));
+   filename = char(input.filename(wi));
    zterm = input.zterm(wi);
    filterCount = input.filterCount(wi);
    alignP = input.alignP(wi);
    mFactor = input.mFactor(wi);
-   delta = input.delta(wi);
-   downSample = input.downSample(wi);
+   
    tt = char(strcat(folder,'-',dateN,'-',filename));
    fn = char(strcat(dataPath,folder,'\',filename));
-   %read in big file
    M = csvread(fn,nh,0);
+
+   %Tin = 5000000/20; %knowing how many peaks;
    max2 = max(M(:,2));
    filterValue = filterCount * max2 /256
    min2 = min(M(:,2));
@@ -64,24 +64,19 @@ for wi = 1:n
    %set end point
    lastP = max(lastMax,lastMin);
    %plot alignment and p
-   t1 = max(1,fstMax - 2*delta);
+   t1 = fstMax - 2*delta;
    t2 = fstMax + 8*delta;
    %t1 = max(1,fstMax - 0.5*T);
    %t2 = fstMax + T;
    %find a point of percent to align three waves
-   v1 = 0;
-   v2s = 0;
-   v3s = 0;
-   if alignP > 0 %do alignment
+   %if alignV > 0
    alignV = alignP*M(fstMax,2);
-   if false
-   %figure
-   %hold on;
-   %plot(M(1:fstMax+T,1),M(1:fstMax+T,2),M(1:fstMax+T,1),M(1:fstMax+T,3),M(1:fstMax+T,1),M(1:fstMax+T,4))
-   %x=[M(fstMax,1),M(fstMax,1)];
-   %plot(x,y1,'black','linewidth',1);
-   %hold off;
-   end
+   figure
+   hold on;
+   plot(M(1:fstMax+T,1),M(1:fstMax+T,2),M(1:fstMax+T,1),M(1:fstMax+T,3),M(1:fstMax+T,1),M(1:fstMax+T,4))
+   x=[M(fstMax,1),M(fstMax,1)];
+   plot(x,y1,'black','linewidth',1);
+   hold off;
    %find the 10% alignment for v2 
    [c1 v1] = min(abs(M(fstMax-delta:fstMax+delta,2)-alignV));
    [c2 v2] = min(abs(M(fstMax-delta:fstMax+delta,3)-alignV));
@@ -96,7 +91,7 @@ for wi = 1:n
        v2s = 0;
        v3s = 0;
    end    
-   end
+  % end
    %make plot 
 
    MM =M(firstP:lastP,2:4);
@@ -118,58 +113,29 @@ for wi = 1:n
    %size(MM(1+v3s:end,3))
    %size(deltaV(1:end-v3s+v2s))
    P = mean(abs(deltaV(1:end-v3s+v2s).*MM(1+v3s:end,3)))/zterm*0.94;
-   T1 =[T1;table(folder,dateN,filename,T,zterm,yrms(1),yrms(2),yrms(3),P0,P,filterValue,...
-    'VariableName',{'folder','date','filename','T','Zterm','v1rms','v2rms','v3rms','CoreQPow','P','noise'})];
 
-   p1 = char(strcat('RMS Voltage Method: (rms(v1)-rms(v2))*rms(v3)/Z = ',num2str(P0),...
-       ' rms(v1) = ',num2str(yrms(1)), ' rms(v2) = ',num2str(yrms(2)), ' rms(v3) = ',num2str(yrms(3))));
-   
-   p2 = char(strcat('Pulse Alignment Method: mean(abs(v1-v2)*v3)/Z = ',num2str(P),' Z=', num2str(zterm)));
-   p3 = char(strcat('max(v1) = ', num2str(max2),' min(v1) = ', num2str(min2)));
-   f2 = figure('Position',pos);
-   subplot(3,1,1);
-   suptitle(tt); 
-   grid on;
-   grid minor;
-   plot(M(:,1),M(:,2),M(:,1),M(:,3),M(:,1),M(:,4))
-    
-   subplot(3,1,2);
-   grid on;
-   grid minor;
-   hold on;
-   annotation('textbox',[0.14,0.78,0.1,0.1],'String',p3,'FitBoxToText','on');
-   x=[M(firstP,1),M(firstP,1)];
-   plot(M(1:2*T,1),M(1:2*T,2),M(1:2*T,1),M(1:2*T,3),M(1:2*T,1),M(1:2*T,4))
-   
-   plot(x,y,'black');
-   text(x(1),y(2),'trim first pulse before \leftarrow','HorizontalAlignment','right');
-   hold off;
-   %set(gca,'XTick',[]);
-   subplot(3,1,3);
-   grid on;
-   grid minor;
-   x=[M(lastP,1),M(lastP,1)];
-   hold on;
-   plot(M(end-2*T:end,1),M(end-2*T:end,2),M(end-2*T:end,1),M(end-2*T:end,3),M(end-2*T:end,1),M(end-2*T:end,4))
-   plot(x,y,'black');
-   text(x(1),y(2),'\rightarrow trim last pulse after');
-   hold off;
-   export_fig(f2,figname,'-append'); 
+%   T1 =[T1;table(folder,dateN,filename,T,zterm,yrms(1),yrms(2),yrms(3),P0,P,filterValue,...
+ %   'VariableName',{'folder','date','filename','T','Zterm','v1rms','v2rms','v3rms','CoreQPow','P','noise'})];
 
+   pnote = [char(strcat('RMS Voltage Method: (rms(v1)-rms(v2))*rms(v3)/R = ',num2str(P0))),...
+   char(strcat('Pulse Alignment Method: mean(abs(v1-v2)*v3)/R = ',num2str(P), ' with noise reduced by ', filterValue))];  
+   p1 = char(strcat('RMS Voltage Method: (rms(v1)-rms(v2))*rms(v3)/R = ',num2str(P0)));
+   p2 = char(strcat('Pulse Alignment Method: mean(abs(v1-v2)*v3)/R = ',num2str(P)));
+ 
    f1 = figure('Position',pos);
-   if (v2s > 0 && v3s > 0)
+   
    subplot(3,1,1);
    grid on;
    grid minor;
    %hold on;
    suptitle(tt); 
-   x = [M(fstMax,1),M(fstMax,1)];
-   dim = [0.14 0.645 0.1 0.1];
-   annotation('textbox',dim,'String',p1,'FitBoxToText','on');
+   x=[M(fstMax,1),M(fstMax,1)];
+   %dim = [0.15 0.25 0.5 0.5];
+   %annotation('textbox',dim,'String',p1,'FitBoxToText','on');
    hold on;
    plot(M(t1:t2,1),M(t1:t2,2),M(t1:t2,1),M(t1:t2,3),M(t1:t2,1),M(t1:t2,4))
    %plot(x,y1);
-   %xlabel(p1);
+   xlabel(p1);
    hold off;
    legend('v1','v2','v3');
    
@@ -182,14 +148,12 @@ for wi = 1:n
    x=[M(fstMax-delta+v1,1),M(fstMax-delta+v1,1)];
    plot(M(t1:t2,1),M(t1:t2,2),M(t1:t2-v2s,1),M(t1+v2s:t2,3),M(t1:t2-v3s,1),M(t1+v3s:t2,4))
    plot(x,y1,'black','linewidth',1);
-   text(x(1),alignV,'\leftarrow aligned at 10% pulse amplitude');
-   
-   %xlabel(p2);
+   text(x(1),y1(1),'\leftarrow aligned at 10% pulse amplitude');
+   xlabel(p2);
    hold off;
-   dim2 = [0.14 0.35 0.1 0.1];
    % dim = [0.15 0.25 0.5 0.5];
  
-   annotation('textbox',dim2,'String',p2,'FitBoxToText','on');
+   %annotation('textbox',dim,'String',p2,'FitBoxToText','on');
    
    legend('v1','v2','v3');
    set(gca,'XTick',[]);
@@ -204,50 +168,39 @@ for wi = 1:n
    yyaxis right
    plot(M(t1:t2-v3s,1),P);
    ylabel('P');
+   export_fig(f1,figname,'-append');
    
-   else
-   Ms = M(1 : downSample : end,:);
-   t1s = max(1,fix(t1/downSample));
-   t2s = fix(t2/downSample);
-   subplot(2,1,1);
-   grid on;
-   grid minor;
-   %hold on;
-   suptitle(tt); 
-   
-   dim = [0.14 0.52 0.1 0.1];
-   annotation('textbox',dim,'String',p1,'FitBoxToText','on');
-   hold on;
-   plot(Ms(t1s:t2s,1),Ms(t1s:t2s,2),Ms(t1s:t2s,1),Ms(t1s:t2s,3),Ms(t1s:t2s,1),Ms(t1s:t2s,4))
-   %plot(x,y1);
-   %xlabel(p1);
-   hold off;
-   legend('v1','v2','v3');
-   
-   %ylabel('V'); 
-   set(gca,'XTick',[]);
-
-   dim = [0.14 0.05 0.1 0.1];
-   % dim = [0.15 0.25 0.5 0.5];
- 
-   annotation('textbox',dim,'String',p2,'FitBoxToText','on');
-   
-   legend('v1','v2','v3');
-   set(gca,'XTick',[]);
-   subplot(2,1,2);
-   grid on;
-   grid minor;
-   deltaV = (Ms(t1s:t2s,2)-Ms(t1s:t2s,3));
-   P = deltaV(1:end).*Ms(t1s:t2s,4)/zterm*deltat;
-   yyaxis left
-   plot(Ms(t1s:t2s,1),deltaV);
-   ylabel('v1-v2');
-   yyaxis right
-   plot(Ms(t1s:t2s,1),P);
-   ylabel('P');  
    %vpexp = expFit1(M(fstMax:fstMax+delta,2:4),figname,pos,2)
-   end
-export_fig(f1,figname,'-append');
+   
+   f2 = figure('Position',pos);
+   subplot(3,1,1);
+   suptitle(tt); 
+   grid on;
+   grid minor;
+   plot(M(:,1),M(:,2),M(:,1),M(:,3),M(:,1),M(:,4))
+   subplot(3,1,2);
+   grid on;
+   grid minor;
+   hold on;
+   
+   x=[M(firstP,1),M(firstP,1)];
+   plot(M(1:2*T,1),M(1:2*T,2),M(1:2*T,1),M(1:2*T,3),M(1:2*T,1),M(1:2*T,4))
+   
+   plot(x,y,'black','linewidth',1);
+   text(x(1),y(2),'trim first pulse before \leftarrow','HorizontalAlignment','right');
+   hold off;
+   %set(gca,'XTick',[]);
+   subplot(3,1,3);
+   grid on;
+   grid minor;
+   x=[M(lastP,1),M(lastP,1)];
+   hold on;
+   plot(M(end-2*T:end,1),M(end-2*T:end,2),M(end-2*T:end,1),M(end-2*T:end,3),M(end-2*T:end,1),M(end-2*T:end,4))
+   plot(x,y,'black','linewidth',1);
+   text(x(1),y(2),'\rightarrow trim last pulse after');
+   hold off;
+   export_fig(f2,figname,'-append'); 
+
 end
-writetable(T1,filen1);
+%writetable(T1,filen1);
 
