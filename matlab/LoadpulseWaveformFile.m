757e2a5b0b4f39c0110885b5d792fb2a6195cda7
jliu4 <jliuapril@gmail.com>
2017-04-17 09:31:24 -0700

    waveform

--- matlab/LoadpulseWaveformFile.m
+++ matlab/LoadpulseWaveformFile.m
@@ -5,21 +5,27 @@ dataPath = 'D:\DropBox\Dropbox (BEC)\BECteam\Jin\waveform\';
 fn = char(strcat(dataPath,'waveform.xlsx'));
 waveform = readtable(fn);
 input = [waveform(1,:);waveform(3,:);waveform(6,:);waveform(8:9,:)];
+%input = [waveform(11:12,:)];
+%10:10 3.722723	8.649782	7.514548	7.048463
 input.folder(1);
 n = size(input,1);
 %waveform = xlsread('waveform.xlsx');
 %factor of min or max
 vfactor = 0.94;% off 0.6 seconds for q-pulse measurement.
 totalTime = 0.002;
+timePerSample = 0.4; %0.4ns second
 nh = 21; %n header
 %10% from the peak value
-
+inchNs = 0.0847253; % speed light
+coreL = 16.5; %inch
 deltat = 1.00000000012417E-09;
 outputPath ='C:\jinwork\BEC\tmp\';
-filen1 = strcat(outputPath,'waveform_041217-11.csv');
+filen1 = strcat(outputPath,'waveform_041417-ipb3-37.csv');
+
+
 T1=cell2table(cell(0,11),...
 'VariableName',{'folder','date','filename','T','Zterm','v1rms','v2rms','v3rms','CoreQPow','P','noise'});
-figname = strcat(outputPath,'waveform_041217-11.pdf');
+figname = strcat(outputPath,'waveform_041417-ipb3-371.pdf');
 delete(figname);
 pos = [10 10 1000 800];
 for wi = 1:n
@@ -35,10 +41,18 @@ for wi = 1:n
    tt = char(strcat(folder,'-',dateN,'-',filename));
    fn = char(strcat(dataPath,folder,'\',filename));
    %read in big file
+   
    M = csvread(fn,nh,0);
-   max2 = max(M(:,2));
-   filterValue = filterCount * max2 /256
-   min2 = min(M(:,2));
+   [row, col] = find(isnan(M(:,2:4)))
+   M(end,1)-M(1,1)
+   vpa(M(1:10,1))
+   max2 = max(M(:,4))
+   max2 = max(M(:,3))
+   %max2 = max(M(:,2))
+   filterValue = filterCount * max2 /128 %256?
+   min2 = min(M(:,4))
+   %min2 = min(M(:,3))
+   %min2 = min(M(:,2))
    y1 = [0,max2(1)];
    y = [min2(1),max2(1)];
    maxP = find(M(:,2) > mFactor*max2);
@@ -64,8 +78,10 @@ for wi = 1:n
    %set end point
    lastP = max(lastMax,lastMin);
    %plot alignment and p
-   t1 = max(1,fstMax - 2*delta);
+   %t1 = max(1,fstMax - 2*delta);
+   t1 = max(1,fstMax - 8*delta);
    t2 = fstMax + 8*delta;
+  
    %t1 = max(1,fstMax - 0.5*T);
    %t2 = fstMax + T;
    %find a point of percent to align three waves
@@ -91,6 +107,7 @@ for wi = 1:n
    %M(fstMax-delta+v3,4)
    %v1,v2,v3
    v2s = v2-v1;
+   c = coreL/(v2s*timePerSample)*inchNs;
    v3s = v3-v1;
    if v2s <0 || v3s < 0
        v2s = 0;
@@ -117,14 +134,16 @@ for wi = 1:n
    %size(deltaV)
    %size(MM(1+v3s:end,3))
    %size(deltaV(1:end-v3s+v2s))
-   P = mean(abs(deltaV(1:end-v3s+v2s).*MM(1+v3s:end,3)))/zterm*0.94;
+   %P = mean(abs(deltaV(1:end-v3s+v2s).*MM(1+v3s:end,3)))/zterm;
+   P =mean(deltaV(1:end-v3s+v2s).*MM(1+v3s:end,3))/zterm
+   
    T1 =[T1;table(folder,dateN,filename,T,zterm,yrms(1),yrms(2),yrms(3),P0,P,filterValue,...
     'VariableName',{'folder','date','filename','T','Zterm','v1rms','v2rms','v3rms','CoreQPow','P','noise'})];
 
    p1 = char(strcat('RMS Voltage Method: (rms(v1)-rms(v2))*rms(v3)/Z = ',num2str(P0),...
        ' rms(v1) = ',num2str(yrms(1)), ' rms(v2) = ',num2str(yrms(2)), ' rms(v3) = ',num2str(yrms(3))));
    
-   p2 = char(strcat('Pulse Alignment Method: mean(abs(v1-v2)*v3)/Z = ',num2str(P),' Z=', num2str(zterm)));
+   p2 = char(strcat('Pulse Alignment Method: mean((v1-v2)*v3)/Z = ',num2str(P),' Z=', num2str(zterm), ' propagate speed = ',num2str(c),'c ratio = ',num2str(P/P0) ));
    p3 = char(strcat('max(v1) = ', num2str(max2),' min(v1) = ', num2str(min2)));
    f2 = figure('Position',pos);
    subplot(3,1,1);
@@ -186,10 +205,10 @@ for wi = 1:n
    
    %xlabel(p2);
    hold off;
-   dim2 = [0.14 0.35 0.1 0.1];
+   dim = [0.14 0.35 0.1 0.1];
    % dim = [0.15 0.25 0.5 0.5];
  
-   annotation('textbox',dim2,'String',p2,'FitBoxToText','on');
+   annotation('textbox',dim,'String',p2,'FitBoxToText','on');
    
    legend('v1','v2','v3');
    set(gca,'XTick',[]);
